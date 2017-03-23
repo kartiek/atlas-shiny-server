3ff1e834301ebf2f8cc31ff878374dd16b7408b8
kartiek <kartiek.kanduri@gmail.com>
2017-03-23 13:05:46 +0200

    added faceting

--- expressGene/app.R
+++ expressGene/app.R
@@ -11,7 +11,7 @@ library(plotly)
 library(shinythemes)
 
 # Read the data
-expData <- dplyr::ungroup(readRDS('ThCell.rds'))
+expData <- readRDS('ThCell.rds')
 expGenes <- unique(expData$geneSymbol)
 
 # Define UI for application 
@@ -21,14 +21,19 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                 fluidRow(
                   plotlyOutput("plot1")),
                 fluidRow(
-                  column(3,
+                  column(1,
+                         radioButtons(
+                           'tPlot',h4('Choose plot type'),
+                           choices = c('Long' = 1, 'Faceted' = 2),
+                           selected = 1)),
+                  column(2,
                          selectizeInput(
                            'genes', h4('Type your gene(s) of choice'),
                            choices = NULL, multiple = TRUE)),
                   column(2,
                          checkboxGroupInput(
                            'timeGroup', label = h4('Select time points'), 
-                           choices = c('0h' = '0H', '0.5h' = '05H', '1h' = '1H',
+                           choices = c('0h (Thp)' = '0H', '0.5h' = '05H', '1h' = '1H',
                                        '2h' = '2H', '4h' = '4H', '6h' = '6H',
                                        '12h' = '12H', '24h' = '24H', '48h' = '48H',
                                        '72h' = '72H'),
@@ -39,23 +44,20 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                            'thGroup', label = h4('Select samples (Th1 & Th2)'),
                            choices = c('Th1' = 'Th1',
                                        'Th2' = 'Th2',
-                                       'Th0' = 'Th0-012',
-                                       'Thp' = 'Thp-012'),
-                           selected = c('Th1','Th2','Th0-012','Thp-012'))),
+                                       'Th0' = 'Th0-012'),
+                           selected = c('Th1','Th2','Th0-012'))),
                   column(2,
                          checkboxGroupInput(
                            'th17Group', label = h4('Select samples (Th17)'),
                            choices = c('Th17' = 'Th17',
-                                       'Th0' = 'Th0-17',
-                                       'Thp' = 'Thp-17'),
-                           selected = c('Th17','Th0-17','Thp-17'))),
+                                       'Th0' = 'Th0-17'),
+                           selected = c('Th17','Th0-17'))),
                   column(2,
                          checkboxGroupInput(
                            'tregGroup', label = h4('Select samples (iTreg)'),
                            choices = c('iTreg' = 'iTreg',
-                                       'Th0' = 'Th0-iTr',
-                                       'Thp' = 'Thp-iTr'),
-                           selected = c('iTreg','Th0-iTr','Thp-iTr'))),
+                                       'Th0' = 'Th0-iTr'),
+                           selected = c('iTreg','Th0-iTr'))),
                   column(1,actionButton("go", "Plot"))
                   )
                 )
@@ -68,28 +70,41 @@ server <- function(input, output, session) {
   
   #Reactive expression to get the filtered data 
   expDataTemp <- eventReactive(input$go, {
+    if(input$tPlot == 1){
     fTest <- dplyr::filter(expData,geneSymbol %in% input$genes & timP %in% input$timeGroup &
                       subset %in% c(input$thGroup,input$th17Group,input$tregGroup)) %>% 
       unite(sub_tim,subset,timP,sep='_')
     colnames(fTest) <- c('Sample','geneSymbol','maxExp','minExp','meanExp')
-    fTest$Sample <- factor(fTest$Sample,levels = c('Thp-012_0H','Thp-17_0H','Thp-iTr_0H',
-                                                   'Th0-012_05H','Th0-012_1H','Th0-012_2H','Th0-012_4H','Th0-012_6H','Th0-012_12H','Th0-012_24H','Th0-012_48H','Th0-012_72H',
-                                                   'Th1_05H','Th1_1H','Th1_2H','Th1_4H','Th1_6H','Th1_12H','Th1_24H','Th1_48H','Th1_72H',
-                                                   'Th2_05H','Th2_1H','Th2_2H','Th2_4H','Th2_6H','Th2_12H','Th2_24H','Th2_48H','Th2_72H',
-                                                   'Th0-17_05H','Th0-17_1H','Th0-17_2H','Th0-17_4H','Th0-17_6H','Th0-17_12H','Th0-17_24H','Th0-17_48H','Th0-17_72H',
-                                                   'Th17_05H','Th17_1H','Th17_2H','Th17_4H','Th17_6H','Th17_12H','Th17_24H','Th17_48H','Th17_72H',
-                                                   'Th0-iTr_05H','Th0-iTr_1H','Th0-iTr_2H','Th0-iTr_4H','Th0-iTr_6H','Th0-iTr_12H','Th0-iTr_24H','Th0-iTr_48H','Th0-iTr_72H',
-                                                   'iTreg_05H','iTreg_1H','iTreg_2H','iTreg_4H','iTreg_6H','iTreg_12H','iTreg_24H','iTreg_48H','iTreg_72H'))
+    fTest$Sample <- factor(fTest$Sample,levels = c('Th0-012_0H','Th0-012_05H','Th0-012_1H','Th0-012_2H','Th0-012_4H','Th0-012_6H','Th0-012_12H','Th0-012_24H','Th0-012_48H','Th0-012_72H',
+                                                   'Th1_0H','Th1_05H','Th1_1H','Th1_2H','Th1_4H','Th1_6H','Th1_12H','Th1_24H','Th1_48H','Th1_72H',
+                                                   'Th2_0H','Th2_05H','Th2_1H','Th2_2H','Th2_4H','Th2_6H','Th2_12H','Th2_24H','Th2_48H','Th2_72H',
+                                                   'Th0-17_0H','Th0-17_05H','Th0-17_1H','Th0-17_2H','Th0-17_4H','Th0-17_6H','Th0-17_12H','Th0-17_24H','Th0-17_48H','Th0-17_72H',
+                                                   'Th17_0H','Th17_05H','Th17_1H','Th17_2H','Th17_4H','Th17_6H','Th17_12H','Th17_24H','Th17_48H','Th17_72H',
+                                                   'Th0-iTr_0H','Th0-iTr_05H','Th0-iTr_1H','Th0-iTr_2H','Th0-iTr_4H','Th0-iTr_6H','Th0-iTr_12H','Th0-iTr_24H','Th0-iTr_48H','Th0-iTr_72H',
+                                                   'iTreg_0H','iTreg_05H','iTreg_1H','iTreg_2H','iTreg_4H','iTreg_6H','iTreg_12H','iTreg_24H','iTreg_48H','iTreg_72H'))
     fTest
-    })
+    }
+    else if(input$tPlot == 2){
+      fTest <- dplyr::filter(expData,geneSymbol %in% input$genes & timP %in% input$timeGroup &
+                               subset %in% c(input$thGroup,input$th17Group,input$tregGroup))
+      colnames(fTest) <- c('subset','geneSymbol','timP','maxExp','minExp','meanExp')
+      fTest$timP <- factor(fTest$timP,levels = c('0H','05H','1H','2H','4H','6H','12H','24H','48H','72H'))
+      fTest
+    }})
   
   #Reactive expression with plotly plot
-  ppt <- reactive({
+  ppt <- eventReactive(input$go, {
+    if(input$tPlot == 1){
     p <- expDataTemp() %>% ggplot(data=.) + geom_line(aes(Sample,meanExp,group=1,col=geneSymbol)) +
       geom_ribbon(aes(x=Sample,ymin=minExp,ymax=maxExp,group=1,fill=geneSymbol),alpha=0.3) +
       theme_minimal() + labs(y='Expression',x=NULL) +
       theme(axis.text.x=element_text(angle=65, hjust=1),axis.ticks=element_blank())
-    ggplotly(p,tooltip = c('Sample','meanExp','geneSymbol','maxExp','minExp'))
+    ggplotly(p,tooltip = c('Sample','meanExp','geneSymbol','maxExp','minExp'))}
+    else if(input$tPlot == 2){
+      p <- expDataTemp() %>% ggplot(data=.) + geom_line(aes(timP,meanExp,group=subset,col=subset)) +
+        theme_minimal() + labs(y='Expression',x=NULL) + facet_wrap(~geneSymbol,scales = 'free') +
+        theme(axis.ticks=element_blank())
+      ggplotly(p,tooltip = c('meanExp','subset'))}
   })
   
   output$plot1 <- renderPlotly({
@@ -98,7 +113,7 @@ server <- function(input, output, session) {
     }
     ppt()
   })
-  }
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
